bbc0aaff7a42f44ac6aabb77f03aeb92f8469034
OlegV <oleg.volter@gmail.com>
2024-12-06 20:14:54 +0200

    swapped lines

--- api/server.js
+++ api/server.js
@@ -28,12 +28,12 @@ const app = express();
 const port = 3000;
 const saltRounds = 10;
 
-app.use(express.json());
 app.use(cors({
   origin: 'https://carpp.online', 
   methods: 'GET, POST, PUT, DELETE, OPTIONS',
   allowedHeaders: 'Content-Type, Authorization'
 }));
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
