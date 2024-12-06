390a6af44eef2042a4b4e38b4d0a5d18f5f4bef5
OlegV <oleg.volter@gmail.com>
2024-12-06 14:44:05 +0200

    added headers

--- app-api/server.js
+++ app-api/server.js
@@ -34,6 +34,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.get('/', (req, res) => res.send('success'))
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
