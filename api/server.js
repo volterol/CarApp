eaf951157f6e0138dd77929b1f42bd3fec459836
OlegV <oleg.volter@gmail.com>
2024-12-06 16:10:05 +0200

    changed prefligt options

--- api/server.js
+++ api/server.js
@@ -38,6 +38,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.options('*', cors());
 
 app.get('/api', (req, res) => res.send('success'))
 app.post('/api/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) });
